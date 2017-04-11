7f1a1dc6326248b0b985229cef5d98fae74e923a
WIN\vcaropr1 <vcaropr1@jhmi.edu>
2017-04-11 14:22:44 -0400

    Adjust variable name to be appropriate and add README

--- Post_Multi_Sample_Processing/SCRIPTS/00_SUBMITTER.MULTI.SAMPLE.REPORTS.sh
+++ Post_Multi_Sample_Processing/SCRIPTS/00_SUBMITTER.MULTI.SAMPLE.REPORTS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PROJECT=$1 # Just the project name.
-MDNA_HASH_ADDRESS=$2 # should be "DRAGEN_OUTPUT"  I didn't change the variable name to match appropriately.  
-SAMPLE_SHEET=$3
+SAMPLE_SHEET=$2
 
 TIMESTAMP=`date '+%F.%H-%M-%S'`
 SCRIPT_DIR="/isilon/sequencing/VITO/NEW_GIT_REPO/EDICO_SCRIPTS/Post_Multi_Sample_Processing/SCRIPTS/"
+DRAGEN_FOLDER="DRAGEN_OUTPUT" 
 # set
 
 
@@ -21,7 +21,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-o","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$1".GENOTYPE_REFINEMENT.MS.log",\
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$1".GENOTYPE_REFINEMENT.MS.log",\
 "'$SCRIPT_DIR'""/C.00_GENOTYPE_REFINEMENT.sh",\
-$1,$2,$3,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 # # ADD/UPDATE INFO ANNOTATIONS
 
@@ -36,7 +36,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$1".REFINED_VARIANT_ANNOTATION.MS.log",\
 "-hold_jid C.00_GENOTYPE_REFINEMENT."$1,\
 "'$SCRIPT_DIR'""/C.00.A.01_REFINED_VARIANT_ANNOTATION.sh",\
-$1,$2,$3,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 
 sed 's/\r//g' $SAMPLE_SHEET \
@@ -50,7 +50,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.VCF.WHOLE.GENOME.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.01_EXTRACT_VCF_WHOLE_GENOME.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -63,7 +63,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.VCF.CODING.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.02_EXTRACT_VCF_CODING.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -76,7 +76,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.VCF.WHOLE.GENOME.PASS.VARIANT.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.03_EXTRACT_VCF_WHOLE_GENOME_PASS_VARIANT.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -89,7 +89,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.VCF.CODING.PASS.VARIANT.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.04_EXTRACT_VCF_CODING_PASS_VARIANT.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -102,7 +102,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.SNV.WHOLE.GENOME.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.05_EXTRACT_SNV_WHOLE_GENOME.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -115,7 +115,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.SNV.CODING.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.06_EXTRACT_SNV_CODING.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -128,7 +128,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.SNV.WHOLE.GENOME.PASS.VARIANT.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.07_EXTRACT_SNV_WHOLE_GENOME_PASS_VARIANT.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -141,7 +141,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".TITV.WHOLE.GENOME.PASS.MS.log",\
 "-hold_jid C.07_EXTRACT_SNV_WHOLE_GENOME_PASS_VARIANT."smtag[1]"_"smtag[2],\
 "'$SCRIPT_DIR'""/C.07.A.01_TI_TV_WHOLE_GENOME_PASS.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -154,7 +154,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".TITV.WHOLE.GENOME.KNOWN.MS.log",\
 "-hold_jid C.07_EXTRACT_SNV_WHOLE_GENOME_PASS_VARIANT."smtag[1]"_"smtag[2],\
 "'$SCRIPT_DIR'""/C.07.A.02_TI_TV_WHOLE_GENOME_KNOWN.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -167,7 +167,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".TITV.WHOLE.GENOME.NOVEL.MS.log",\
 "-hold_jid C.07_EXTRACT_SNV_WHOLE_GENOME_PASS_VARIANT."smtag[1]"_"smtag[2],\
 "'$SCRIPT_DIR'""/C.07.A.03_TI_TV_WHOLE_GENOME_NOVEL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 # Commented out concordance.  This step takes an absurd amount of time to run on WG data.  Not worth it per last conversation.
 
@@ -182,7 +182,7 @@ $1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}'
 # "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".CONCORDANCE.WHOLE.GENOME.MS.log",\
 # "-hold_jid C.07_EXTRACT_SNV_WHOLE_GENOME_PASS_VARIANT."smtag[1]"_"smtag[2],\
 # "'$SCRIPT_DIR'""/C.07.A.04_CONCORDANCE_WHOLE_GENOME.sh",\
-# $1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+# $1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -195,7 +195,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.SNV.CODING.PASS.VARIANT.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.08_EXTRACT_SNV_CODING_PASS_VARIANT.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -208,7 +208,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".TITV.CODING.PASS.MS.log",\
 "-hold_jid C.08_EXTRACT_SNV_CODING_PASS_VARIANT."smtag[1]"_"smtag[2],\
 "'$SCRIPT_DIR'""/C.08.A.01_TI_TV_CODING_PASS.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -221,7 +221,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".TITV.CODING.KNOWN.MS.log",\
 "-hold_jid C.08_EXTRACT_SNV_CODING_PASS_VARIANT."smtag[1]"_"smtag[2],\
 "'$SCRIPT_DIR'""/C.08.A.02_TI_TV_CODING_KNOWN.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -234,7 +234,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".TITV.CODING.NOVEL.MS.log",\
 "-hold_jid C.08_EXTRACT_SNV_CODING_PASS_VARIANT."smtag[1]"_"smtag[2],\
 "'$SCRIPT_DIR'""/C.08.A.03_TI_TV_CODING_NOVEL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -247,7 +247,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.INDEL.WHOLE.GENOME.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.09_EXTRACT_INDEL_WHOLE_GENOME.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -260,7 +260,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.INDEL.CODING.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.10_EXTRACT_INDEL_CODING.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -273,7 +273,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.INDEL.WHOLE.GENOME.PASS.VARIANT.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.11_EXTRACT_INDEL_WHOLE_GENOME_PASS_VARIANT.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -286,7 +286,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.INDEL.CODING.PASS.VARIANT.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.12_EXTRACT_INDEL_CODING_PASS_VARIANT.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 # Commented out because dragen doesn't have fun vqsr plot stuff
 
@@ -300,7 +300,7 @@ $1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}'
 # "-o","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".VQSR.SNP.PLOT.MS.log",\
 # "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".VQSR.SNP.PLOT.MS.log",\
 # "'$SCRIPT_DIR'""/C.13_VQSR_SNP_PLOT_MS.sh",\
-# $1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+# $1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 # 
 # sed 's/\r//g' $SAMPLE_SHEET \
 # | awk 'NR>1' \
@@ -312,7 +312,7 @@ $1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}'
 # "-o","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".VQSR.INDEL.PLOT.MS.log",\
 # "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".VQSR.INDEL.PLOT.MS.log",\
 # "'$SCRIPT_DIR'""/C.14_VQSR_INDEL_PLOT_MS.sh",\
-# $1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+# $1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -325,7 +325,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.VCF.WHOLE.GENOME.PASS.ALL.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.15_EXTRACT_VCF_WHOLE_GENOME_PASS_ALL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -338,7 +338,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.VCF.CODING.PASS.ALL.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.16_EXTRACT_VCF_CODING_PASS_ALL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -351,7 +351,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.INDEL.WHOLE.GENOME.PASS.ALL.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.17_EXTRACT_INDEL_WHOLE_GENOME_PASS_ALL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -364,7 +364,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.INDEL.CODING.PASS.ALL.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.18_EXTRACT_INDEL_CODING_PASS_ALL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -377,7 +377,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.SNV.WHOLE.GENOME.PASS.ALL.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.19_EXTRACT_SNV_WHOLE_GENOME_PASS_ALL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -390,7 +390,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$2".EXTRACT.SNV.CODING.PASS.ALL.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.20_EXTRACT_SNV_CODING_PASS_ALL.sh",\
-$1,$2,$3,$4,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,$4,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 
 sed 's/\r//g' $SAMPLE_SHEET \
@@ -404,7 +404,7 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$1".BGZIP_REFINED_VCF.MS.log",\
 "-hold_jid C.00.A.01_REFINED_VARIANT_ANNOTATION."$1,\
 "'$SCRIPT_DIR'""/C.21_BGZIP_REFINED_VCF.sh",\
-$1,$2,$3,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
 
 sed 's/\r//g' $SAMPLE_SHEET \
 | awk 'NR>1' \
@@ -417,4 +417,4 @@ sed 's/\r//g' $SAMPLE_SHEET \
 "-e","/isilon/sequencing/Seq_Proj/"$1"/LOGS/"$1".TABIX_REFINED_VCF.MS.log",\
 "-hold_jid C.21_BGZIP_REFINED_VCF."$1,\
 "'$SCRIPT_DIR'""/C.21.A.01_TABIX_REFINED_VCF.sh",\
-$1,$2,$3,"'$MDNA_HASH_ADDRESS'""\n""sleep 3s"}' 
+$1,$2,$3,"'$DRAGEN_FOLDER'""\n""sleep 3s"}' 
